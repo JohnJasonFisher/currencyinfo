ff9045ab50da834f5bfa5b30a51069543e4dc0e1
JohnJasonFisher <jjfisher91@gmail.com>
2022-10-17 16:49:40 -0500

    Deletes AppInfo & Adds one usd imgs

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,5 @@
 <script>
   import Counter from './lib/Counter.svelte'
-  import AppInfo from './lib/AppInfo.svelte'
   import DocumentHead from './lib/DocumentHead.svelte';
 </script>
 
@@ -10,6 +9,7 @@
   <div class="card">
     <DocumentHead />
     <Counter />
-    <AppInfo />
+    <img src="src/assets/currency-images/usd/one-usd-front.png" alt="One United States Dollar Front">
+    <img src="src/assets/currency-images/usd/one-usd-back.png" alt="One United States Dollar Back">
   </div>
 </main>
