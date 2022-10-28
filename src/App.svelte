b2458e1939d6c14aa4068190fc9b0ad23681d391
JohnJasonFisher <jjfisher91@gmail.com>
2022-10-28 17:09:34 -0500

    Shows different currency on button click

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,10 @@
 <script>
 	import CurrencyInfo from './lib/CurrencyInfo.svelte'
 	import DocumentHead from './lib/DocumentHead.svelte'
+
+	let showCurrencyInfo = 'usd'
+	let handleShowUsd = () => showCurrencyInfo = 'usd'
+	let handleShowAwg = () => showCurrencyInfo = 'awg'
 </script>
 
 <main>
@@ -8,13 +12,19 @@
 
 	<div class="card">
 		<DocumentHead />
-		<button>USD</button>
-		<button>AWG</button>
-		<CurrencyInfo currencyDemoninations={['one']} />
-		<CurrencyInfo
-			currencyName={'Aruban Florin'}
-			symbol={'awg'}
-			currencyDemoninations={['five']}
-		/>
+		<button on:click={handleShowUsd}>USD</button>
+		<button on:click={handleShowAwg}>AWG</button>
+
+		{#if showCurrencyInfo === 'usd'}
+			<CurrencyInfo currencyDemoninations={['one']} />
+		{/if}
+
+		{#if showCurrencyInfo === 'awg'}
+			<CurrencyInfo
+				currencyName={'Aruban Florin'}
+				symbol={'awg'}
+				currencyDemoninations={['five']}
+			/>
+		{/if}
 	</div>
 </main>
