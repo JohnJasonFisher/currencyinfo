db89c754315a5eba15721bdba5ea8109cd9b385e
JohnJasonFisher <jjfisher91@gmail.com>
2022-10-29 10:47:15 -0500

    Adds Mexican Peso

--- src/App.svelte
+++ src/App.svelte
@@ -14,6 +14,7 @@
 		<DocumentHead />
 		<button on:click={() => setCurrencyDetailSymbol('usd')}>USD</button>
 		<button on:click={() => setCurrencyDetailSymbol('awg')}>AWG</button>
+		<button on:click={() => setCurrencyDetailSymbol('mxn')}>MXN</button>
 
 		{#if currencyDetailSymbol === 'usd'}
 			<CurrencyInfo currencyDemoninations={['1']} />
@@ -24,6 +25,14 @@
 				currencyName={'Aruban Florin'}
 				symbol={'awg'}
 				currencyDemoninations={['10', '25', '50', '100', '200']}
+			/>
+		{/if}
+
+		{#if currencyDetailSymbol === 'mxn'}
+			<CurrencyInfo
+				currencyName={'Mexican Peso'}
+				symbol={'mxn'}
+				currencyDemoninations={['20', '50', '100', '200', '500']}
 			/>
 		{/if}
 	</div>
--- src/helpers/imgurUrlMapper.js
+++ src/helpers/imgurUrlMapper.js
@@ -1,3 +1,9 @@
+// Copy Pasta Template
+// case '':
+//     return { alt: '', src: '' }
+// case '':
+//     return { alt: '', src: '' }
+//
 export function getImageData(key) {
 	switch (key) {
 		case 'usd-1-front':
@@ -24,6 +30,26 @@ export function getImageData(key) {
 			return { alt: 'awg-200-front', src: 'https://imgur.com/R8Vrqa4.jpeg' }
 		case 'awg-200-back':
 			return { alt: 'awg-200-back', src: 'https://imgur.com/5cYpRxM.jpeg' }
+		case 'mxn-20-front':
+			return { alt: 'mxn-20-front', src: 'https://imgur.com/tzXQ4lC.jpeg' }
+		case 'mxn-20-back':
+			return { alt: 'mxn-20-back', src: 'https://imgur.com/0PtTjkm.jpeg' }
+		case 'mxn-50-front':
+			return { alt: 'mxn-50-front', src: 'https://imgur.com/VOO7qBe.jpeg' }
+		case 'mxn-50-back':
+			return { alt: 'mxn-50-back', src: 'https://imgur.com/d6bl6YC.jpeg' }
+		case 'mxn-100-front':
+			return { alt: 'mxn-100-front', src: 'https://imgur.com/ym9fPGA.jpeg' }
+		case 'mxn-100-back':
+			return { alt: 'mxn-100-back', src: 'https://imgur.com/wRtthB5.jpeg' }
+		case 'mxn-200-front':
+			return { alt: 'mxn-200-front', src: 'https://imgur.com/NS2fTNg.jpeg' }
+		case 'mxn-200-back':
+			return { alt: 'mxn-200-back', src: 'https://imgur.com/fZzVzYm.jpeg' }
+		case 'mxn-500-front':
+			return { alt: 'mxn-500-front', src: 'https://imgur.com/3n7jUp0.jpeg' }
+		case 'mxn-500-back':
+			return { alt: 'mxn-500-back', src: 'https://imgur.com/1nAi7no.jpeg' }
 		default:
 			return null
 	}
